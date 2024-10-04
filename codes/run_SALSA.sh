be23e2bcdad6920aa6a99302ff4da5553ec6465c
Maria Balaguer Montero <90611524+mariabalaguerm@users.noreply.github.com>
2024-10-04 13:40:11 +0200

    update run

--- codes/run_SALSA.sh
+++ codes/run_SALSA.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-export nnUNet_raw="/nfs/rwork/mbalaguer/nnUNet_TALES/nnUNet_raw"
-export nnUNet_preprocessed="/nfs/rwork/mbalaguer/nnUNet_TALES/nnUNet_preprocessed"
-export nnUNet_results="/nfs/rwork/mbalaguer/nnUNet_TALES/nnUNet_results"
-
-
-
 # ABSOLUTE PATH TO THE SCAN TO SEGMENT OR CSV WITH PATHS
 # option 1) single scan
-# PATH_INPUT=/nfs/rnas/projects/TALES/codes/pipeline/example0/scan_1.nii
+# PATH_INPUT=/path_to/scan.nii
+
 # option 2) multiple scans (csv)
-PATH_INPUT=/nfs/rnas/projects/TALES/codes/pipeline/csv_paths.csv
+PATH_INPUT=/path_to/csv.csv
 
 
-source activate /nfs/home/clmbalaguer/anaconda3/envs/total_seg-2
-python /nfs/rnas/projects/TALES/codes/pipeline/SALSA_stepONE.py $PATH_INPUT
+source activate .../envs/SALSA_env1
+python ./SALSA_stepONE.py $PATH_INPUT
 
-source activate /nfs/home/clmbalaguer/anaconda3/envs/nnunet
-python /nfs/rnas/projects/TALES/codes/pipeline/SALSA_stepTWO.py $PATH_INPUT
+source activate .../envs/SALSA_env2
+python ./SALSA_stepTWO.py $PATH_INPUT
