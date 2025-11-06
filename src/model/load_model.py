9752148d838ddcd59cb48b2f0c3caacde64eb389
mariabalaguerm <mariabalaguermontero@gmail.com>
2025-11-06 15:54:26 +0100

    update load_model

--- src/model/load_model.py
+++ src/model/load_model.py
@@ -1,5 +1,12 @@
+import os
+
 def load_model(model_dir):
     """
     Placeholder for compatibility – nnU-Net handles model loading internally
     """
-    return model_dir
+
+    if os.path.exists(model_dir):
+        return model_dir
+    else:
+        print('Model weights are not where they are supposed to be :(')
+        return none
